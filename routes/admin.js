218c56a7a189280517cd9b33adef7f6a5fa754fe
s22012 <s22012@std.it-college.ac.jp>
2024-02-27 16:01:00 +0900

    終わり

--- routes/admin.js
+++ routes/admin.js
@@ -1,65 +1,110 @@
-import express, {request} from "express";
+import express from "express";
 import {PrismaClient} from "@prisma/client";
-
 
 const router = express.Router();
 const prisma = new PrismaClient();
-const loginCheck = (req, res, next) => {
-    if (!req.user) {
+const adminCheck = (req, res, next) => {
+    if (!req.user.isAdmin) {
         return res.status(401).json("Not access")
     }
     next();
 };
-router.get("/list", loginCheck, async (req, res, next) => {
-    const page = req.query.page || 1; // ページ番号のデフォルト値は1
-    const pageSize = 10; // 1ページあたりのアイテム数
-
-    try {
-        const totalCount = await prisma.books.count();
-        const maxPage = Math.ceil(totalCount / pageSize);
-        const books = await prisma.books.findMany({
-            select: {
-                id: true,
-                title: true,
-                author: true,
-                publishDate: true,
-                rental: true
-            },
-            skip: (page - 1) * pageSize,
-            take: pageSize
+//管理者の書籍追加
+router.post("/book/create",adminCheck, async (req, res, next) => {
+    const {isbn13,title,author,publishDate} = req.body;
+    try{
+        const create_book = await prisma.books.create({
+            data: {
+                isbn13: parseFloat(isbn13),
+                title,
+                author,
+                publishDate: new Date(publishDate)
+            }})
+        res.status(201).json({
+            message: "OK",
+            create_book
         });
-
-        res.status(200).json({ books , maxPage});
-    } catch (error) {
-        console.error("Error fetching books:", error);
-        res.status(500).json({ error: "Internal server error" });
+    } catch (e) {
+        switch (e.code) {
+            case "P2002":
+                res.status(400).json({
+                    message: "NG"
+                });
+                break;
+            default:
+                console.error(e);
+                res.status(500).json({
+                    message: "NG"
+                });
+        }
     }
 });
+// 管理者の書籍更新
 
-router.get(`/detail/:id`, loginCheck, async (req, res, next) =>{
-    const detail_book = await prisma.books.findFirst({
-        where:{id: req.params.id} , select: {
-            id: true,
-            isbn13: true,
-            title: true,
-            author: true,
-            publishDate: true,
+router.put("/book/update",adminCheck, async (req, res, next) => {
+    const {bookId,isbn13,title,author,publishDate} = req.body;
+    try{
+        const update_book = await prisma.books.update({
+            where: {id: bookId},
+            data: {
+                isbn13: parseFloat(isbn13),
+                title,
+                author,
+                publishDate: new Date(publishDate)
+            }})
+        res.status(201).json({
+            message: "update!",
+            update_book
+        });
+    } catch (e) {
+        switch (e.code) {
+            case "P2002":
+                res.status(400).json({
+                    message: "update error"
+                });
+                break;
+            default:
+                console.error(e);
+                res.status(500).json({
+                    message: "unknown error"
+                });
         }
-    })
-    const rentalInfo = await prisma.rental.findFirst({
-        where:{userId: req.params.id}, select:{
-            user:{select:{
-                    name: true
-                }
-            },
+    }
+});
+router.get("/rental/current", adminCheck, async (req, res, next) =>{
+    const rental_books = await prisma.rental.findMany({
+        where:{returnDate: null,},
+        select: {
+            id: true,
+            user: {select:{id: true, name: true}},
+            book: {select:{id: true, title: true}},
             rentalDate: true,
             returnDeadline: true
         }
     })
     res.status(200).json({
-            detail_book,
-            rentalInfo
+        rental_books
+    })
+});
+router.get(`/rental/current/:uid`, adminCheck, async (req, res, next) =>{
+    const rental_books = await prisma.rental.findMany({
+        where:{returnDate: null,userId: req.params.uid},
+        select: {
+            user: {select:{id: true, name: true}},
         }
-    )
-})
+    })
+    const rental_Books = await prisma.rental.findMany({
+        where:{returnDate: null, userId: req.params.uid},
+        select:{
+            id: true,
+            book: {select:{id: true, title: true}},
+            rentalDate: true,
+            returnDeadline: true
+        }
+    })
+    res.status(200).json({
+        rental_books,
+        rental_Books
+    })
+});
 export default router;
